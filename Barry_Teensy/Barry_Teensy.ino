820f6cf7447a65ea27b8c151c1d20a29c1d86c07
RobotDogDad <austinh054@gmail.com>
2023-07-11 20:59:08 -0700

    Create Barry_Teensy.ino

---
+++ Barry_Teensy/Barry_Teensy.ino
@@ -0,0 +1,41 @@
+#include <SPI.h>
+#include <nRF24L01.h>
+#include <RF24.h>
+
+#define buttonOut 39
+#define led 32
+
+RF24 radio(9, 10, 4000000); // CE, CSN
+const byte addresses[][6] = {"00002", "00001"};
+
+boolean buttonStateOut = 0;
+boolean buttonStateIn = 0;
+
+void setup() {
+  pinMode(buttonOut, INPUT_PULLUP);
+  pinMode(32, OUTPUT);
+
+  radio.begin();
+  radio.openWritingPipe(addresses[0]); // 00002
+  radio.openReadingPipe(1, addresses[1]); // 00001
+  radio.setPALevel(RF24_PA_MIN);
+}
+
+void loop() {
+  delay(5);
+  radio.startListening();
+
+  radio.read(&buttonStateIn, sizeof(buttonStateIn));
+  if (buttonStateIn == HIGH) {
+    digitalWrite(led, HIGH);
+  }
+  else {
+    digitalWrite(led, LOW);
+  }
+  delay(5);
+
+  radio.stopListening();
+  
+  buttonStateOut = 1-digitalRead(buttonOut);
+  radio.write(&buttonStateOut, sizeof(buttonStateOut));
+}
