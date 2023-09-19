bcf197670f1e51e78ab4d43f416d368b41b8f04d
RobotDogDad <austinh054@gmail.com>
2023-09-18 20:28:05 -0700

    Working servos

--- Barry_Teensy/Barry_Teensy.ino
+++ Barry_Teensy/Barry_Teensy.ino
@@ -1,186 +1,403 @@
+//// Barry the Ringbearer
+
+// Teensy 4.1 Microcontroller, 5V input
+// Vin to logic components 3.3v, Vin to LEDs 5V
+
+//// Pinout
+// Pin 0: RX, ODrive 1
+// Pin 1: TX, ODrive 1
+// Pin 2: PWM, Servo 1
+// Pin 3: PWM, Servo 2
+// Pin 4: PWM, Servo 3
+// Pin 5: PWM, Servo 4
+// Pin 7: RX, ODrive 2
+// Pin 8: TX, ODrive 2
+// Pin 9: Digital Output, Radio CE
+// Pin 10: Digital Output, Radio CS
+// Pin 11: SPI, Radio MOSI
+// Pin 12: SPI, Radio MISO
+// Pin 13: SPI, Radio SCK
+// Pin 14: TX, RESERVED
+// Pin 15: RX, RESERVED
+// Pin 16, TX, RESERVED
+// Pin 17, TX, RESERVED
+// Pin 18: I2C SDA, RESERVED
+// Pin 19: I2C SCK, RESERVED
+// Pin 20: TX, RESERVED
+// Pin 21: RX, RESERVED
+// Pin 24: TX, RESERVED
+// Pin 25: RX, RESERVED
+// Pin 28: RX, RESERVED
+// Pin 29: TX, RESERVED
+// Pin 33: Digital Output, NeoPixels
+// Pin 34: RX, RESERVED
+// Pin 35: TX, RESERVED
+// Pin 39: Analog In, RESERVED
+// Pin 40: Analog In, RESERVED
+// Pin 41: Analog In, RESERVED
+
 // Library Imports
-#include "SparkFunLSM6DSO.h"
-#include "Wire.h"
-#include "math.h"
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Adafruit_NeoPixel.h>
+#include <PWMServo.h>
+#include <DifferentialSteering.h>
+#include <HardwareSerial.h>
+#include <SoftwareSerial.h>
+#include <ODriveArduino.h>
 
-// I/O Definitions
-#define led 32
-#define led_comms_ON 33
-#define led_comms_OFF 34
+// Printing with stream operator helper functions
+template<class T> inline Print& operator <<(Print &obj,     T arg) { obj.print(arg);    return obj; }
+template<>        inline Print& operator <<(Print &obj, float arg) { obj.print(arg, 4); return obj; }
 
-LSM6DSO myIMU; //Default constructor is I2C, addr 0x6B
-
-
-// Instantiate Radio
-RF24 radio(9, 10, 4000000); // CE, CSN, SPEED
-const byte addresses[][6] = {"00002", "00001"};
 
 //Variable Declarations
-////  Sensor values
-float AccelY;
-float AccelZ;
-float AccelAngleX;
-float GyroX;
-float fusedPitch = 0;
+//// Integers
+int Servo1Value;
+int Servo2Value;
+int Servo3Value;
+int Servo4Value;
+int fPivYLimit = 32;
+int LeftMotor;
+int RightMotor;
+int motornum = 0;
+int requested_state;
 
-////  Booleans
-boolean buttonStateIn = 0;
-boolean commState = 0;
+//// Boolean
+
+
+//// Floats
+float StickLXScaled = 0;
+float StickLYScaled = 0;
+float StickRXScaled = 0;
+float StickRYScaled = 0;
+float DriveVel = 0;
+float DriveRot = 0;
 
 ////  Counters
 const unsigned long overflowThreshold = 1000000000UL;
 unsigned long previousTime = 0;
-
-
 unsigned long currentMillis;
 long previousMillis = 0;    // set up timers
 long interval = 10;        // time constant for timer
 unsigned long count;
-
 unsigned long remoteMillis;
 
+
+// Instantiate Peripherals
+////Radio
+RF24 radio(9, 10, 4000000); // CE, CSN
+const byte addresses[][6] = {"00002", "00001"};
+
+////LEDs
+Adafruit_NeoPixel ring = Adafruit_NeoPixel(24, 33, NEO_GRB + NEO_KHZ800);
+
+////Servos
+PWMServo servo1; // Right Elbow
+PWMServo servo2; // Right Shoulder
+PWMServo servo3; // Left Elbow
+PWMServo servo4; // Left Shoulder
+
+////ODrives
+DifferentialSteering DiffSteer;
+HardwareSerial& odrive_serial1 = Serial1;
+HardwareSerial& odrive_serial2 = Serial2;
+
+ODriveArduino odrive1(odrive_serial1);
+ODriveArduino odrive2(odrive_serial2);
 
 // Data Package
 typedef struct
 {
-float corrected_angle;
+int PingBack;
 }
 displayDef;
 displayDef displayPak;
 
 typedef struct
 {
-  int joy1XValue;
-  int joy1YValue;
-  boolean joy1buttonValue;
+  int StickLXValue;
+  int StickLYValue;
+  boolean StickLButtonValue;
+  int StickRXValue;
+  int StickRYValue;
+  boolean StickRButtonValue;
+  boolean BumperLValue;
+  boolean BumperRValue;
+  boolean SelectLValue;
+  boolean SelectRValue;
+  boolean ButtonDValue;
+  boolean ButtonLValue;
+  boolean ButtonRValue;
+  boolean ButtonUValue;
 }
 controlDef;
 controlDef controlPak;
 
-
-
 void setup() {
 
-  pinMode(32, OUTPUT);
-  pinMode(33, OUTPUT);
-  pinMode(34, OUTPUT);
-
-// Initialize Serial, I2C and Radio
+// Initialize Serial, Radio, Servo, LEDs & Drive
   Serial.begin(115200);
-  delay(500); 
-  
-  Wire.begin();
-  delay(10);
 
   radio.begin();
   radio.openWritingPipe(addresses[0]); // 00002
   radio.openReadingPipe(1, addresses[1]); // 00001
   radio.setPALevel(RF24_PA_MAX);
   radio.setChannel(100);
+ 
+  servo1.attach(2);
+  servo2.attach(3);
+  servo3.attach(4);
+  servo4.attach(5);
 
-  // Check for IMU State
-  if( myIMU.begin() ){
-    Serial.println("Ready.");
-  }
-  else { 
-    Serial.println("Could not connect to IMU.");
-    Serial.println("Freezing");
-  }
+  servo1.write(65);
+  servo2.write(60);
+  servo3.write(170);
+  servo4.write(175);  
 
-  if( myIMU.initialize(BASIC_SETTINGS) ){
-    Serial.println("Loaded Settings.");
-  }
+  ring.begin();
+
+  displayPak.PingBack = 1;
+
+  DiffSteer.begin(fPivYLimit);
+
+  // ODrive uses 115200 baud
+  odrive_serial1.begin(115200);
+  odrive_serial2.begin(115200);
+
+odrive_serial1 << "w axis0.controller.config.vel_limit " << 10.0f << '\n';
+odrive_serial1 << "w axis0.motor.config.current_lim " << 10.0f << '\n';
+odrive_serial2 << "w axis0.controller.config.vel_limit " << 10.0f << '\n';
+odrive_serial2 << "w axis0.motor.config.current_lim " << 10.0f << '\n';
+
+      odrive1.run_state(0, 8, false);
+      requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL;
+      //Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      if(!odrive2.run_state(motornum, requested_state, false)) return;
+
+      odrive2.run_state(0, 8, false);
+      requested_state = AXIS_STATE_CLOSED_LOOP_CONTROL;
+      //Serial << "Axis" << c << ": Requesting state " << requested_state << '\n';
+      if(!odrive2.run_state(motornum, requested_state, false)) return;
+    
 
 }
 
 void loop() {
 
-    // Sensor Data Input
-  AccelY = myIMU.readFloatAccelY();
-  AccelZ = myIMU.readFloatAccelZ();
-  GyroX = myIMU.readFloatGyroY();
-
-  // Math
-  //// Compute the time delta (in seconds)
-  unsigned long currentTime = millis();
-  if (currentTime < previousTime) { // Check for overflow
-      currentTime += overflowThreshold;
-    }
-
-  //// Compute Accelerometer Angle
-  AccelAngleX = -atan2(AccelY, AccelZ)*RAD_TO_DEG;
-    //if(isnan(AccelAngleY)) {
-  //  }
-
-  float dt = (currentTime - previousTime) / 1000.0;
-  fusedPitch = 0.98 * (fusedPitch += GyroX * dt) + 0.02 * AccelAngleX;
-  previousTime = currentTime; // Save the current time for the next loop iteration
-
-  //// Wrap around the pitch to keep it within the range of -180 to 180 degrees
-  if (fusedPitch > 180) {
-      fusedPitch -= 360;
-    } else if (fusedPitch < -180) {
-      fusedPitch += 360;
-    }
-  
-    // Save to displayPak
-    displayPak.corrected_angle = (float)fusedPitch;
 
   currentMillis = millis();
   if (currentMillis - previousMillis >= 10) {  // start timed event
 
     previousMillis = currentMillis;
 
-  delay(5);
-  radio.startListening();
+    delay(5);
+    radio.startListening();
 
-      // check for radio data
-  if (radio.available()) {
-      radio.read(&controlPak, sizeof(controlPak));
-      remoteMillis = currentMillis;
+        // Check for radio data
+    if (radio.available()) {
+        radio.read(&controlPak, sizeof(controlPak)); // Read data and record to memory
+        remoteMillis = currentMillis; // Reset Watchdog
+
+/*
+        // Send Data to Serial 
+        Serial.print("Left Stick X value is: ");
+        Serial.println(controlPak.StickLXValue);
+        Serial.print("Left Stick Y value is: ");
+        Serial.println(controlPak.StickLYValue);
+        Serial.print("Right Stick X value is: ");
+        Serial.println(controlPak.StickRXValue);
+        Serial.print("Right Stick Y value is: ");
+        Serial.println(controlPak.StickRYValue);
+
+        Serial.print("Left Stick Button state is: ");
+        Serial.println(controlPak.StickLButtonValue);
+        Serial.print("Right Stick Button state is: ");
+        Serial.println(controlPak.StickRButtonValue);
+
+        Serial.print("Left Bumper state is: ");
+        Serial.println(controlPak.BumperLValue);
+        Serial.print("Right Bumper state is: ");
+        Serial.println(controlPak.BumperRValue);
+
+        Serial.print("Left Select state is: ");
+        Serial.println(controlPak.SelectLValue);
+        Serial.print("Right Select state is: ");
+        Serial.println(controlPak.SelectRValue);
+
+        Serial.print("Down Button state is: ");
+        Serial.println(controlPak.ButtonDValue);
+        Serial.print("Left Button state is: ");
+        Serial.println(controlPak.ButtonLValue);
+        Serial.print("Right Button state is: ");
+        Serial.println(controlPak.ButtonRValue);
+        Serial.print("Up Button state is: ");
+        Serial.println(controlPak.ButtonUValue);
+*/
+      }
+
+      // Is the remote disconnected for too long ?
+    if (currentMillis - remoteMillis > 500) { // Trigger watchdog if data hasn't been received for (500) milliseconds
+        Serial.println("No Connection"); // Watchdog code goes here
     }
 
-    // is the remote disconnected for too long ?
-  if (currentMillis - remoteMillis > 500) {
-      commState = 0;
-      Serial.println("no data");
-  }
-  else {
-    commState = 1;
-    Serial.print("Joystick 1 X Value is: ");
-    Serial.println(controlPak.joy1XValue);
-    Serial.print("Joystick 1 Y Value is: ");
-    Serial.println(controlPak.joy1YValue);
-    Serial.print("Joystick 1 button state is: ");
-    Serial.println(controlPak.joy1buttonValue);
+      // Switch radio from read to write (leave some time here to change modes)
+      delay(5);
+      radio.stopListening();
+      delay(5);
+
+      radio.write( &displayPak, sizeof(displayPak) ); // Send small packet to transmitter for its own internal watchdog
+
+    //End of timed radio loop
   }
 
-  if (buttonStateIn == HIGH) {
-    digitalWrite(led, HIGH);
-  }
-  else {
-    digitalWrite(led, LOW);
+
+
+  ring.setPixelColor(0, ring.Color(5,25,5));
+  ring.setPixelColor(1, ring.Color(5,25,5));
+  ring.setPixelColor(2, ring.Color(5,25,5));
+  ring.setPixelColor(3, ring.Color(5,25,5));
+  ring.setPixelColor(4, ring.Color(5,25,5));
+  ring.setPixelColor(5, ring.Color(5,25,5));
+  ring.setPixelColor(6, ring.Color(5,25,5));
+  ring.setPixelColor(7, ring.Color(5,25,5));
+  ring.setPixelColor(8, ring.Color(5,25,5));
+  ring.setPixelColor(9, ring.Color(5,25,5));
+  ring.setPixelColor(10, ring.Color(5,25,5));
+  ring.setPixelColor(11, ring.Color(5,25,5));
+  ring.setPixelColor(12, ring.Color(5,25,5));
+  ring.setPixelColor(13, ring.Color(5,25,5));
+  ring.setPixelColor(14, ring.Color(5,25,5));
+  ring.setPixelColor(15, ring.Color(5,25,5));
+  ring.setPixelColor(16, ring.Color(5,25,5));    
+  ring.setPixelColor(17, ring.Color(5,25,5));
+  ring.setPixelColor(18, ring.Color(5,25,5));
+  ring.setPixelColor(19, ring.Color(5,25,5));
+  ring.setPixelColor(20, ring.Color(5,25,5));
+  ring.setPixelColor(21, ring.Color(5,25,5));
+  ring.setPixelColor(22, ring.Color(5,25,5));
+  ring.setPixelColor(23, ring.Color(5,25,5));    
+
+ 
+  ring.show();
+
+// Stick Value Conversion
+  StickLXScaled = ((controlPak.StickLXValue - 512.0)/512.0);
+  StickLYScaled = ((controlPak.StickLYValue - 512.0)/512.0);
+  StickRXScaled = ((controlPak.StickRXValue - 512.0)/512.0);
+  StickRYScaled = ((controlPak.StickRYValue - 512.0)/512.0);
+
+  if ((StickLXScaled < 0.1) && (StickLXScaled > -0.1)) {
+    StickLXScaled = 0;
   }
 
-  if (commState == 1) {
-    digitalWrite(led_comms_ON, HIGH);
-    digitalWrite(led_comms_OFF, LOW);
+  if ((StickLYScaled < 0.1) && (StickLYScaled > -0.1)) {
+    StickLYScaled = 0;
   }
-  else {
-    digitalWrite(led_comms_OFF, HIGH);
-    digitalWrite(led_comms_ON, LOW);
-  }
-  delay(5);
 
-  radio.stopListening();
+  if ((StickRXScaled < 0.1) && (StickRXScaled > -0.1)) {
+    StickRXScaled = 0;
+  }
+
+  if ((StickRYScaled < 0.1) && (StickRYScaled > -0.1)) {
+    StickRYScaled = 0;
+  }
+/*
+    // Send Data to Serial 
+    Serial.print("Left Stick X value is: ");
+    Serial.println(StickLXScaled);
+    Serial.print("Left Stick Y value is: ");
+    Serial.println(StickLYScaled);
+    Serial.print("Right Stick X value is: ");
+    Serial.println(StickRXScaled);
+    Serial.print("Right Stick Y value is: ");
+    Serial.println(StickRYScaled);
+
+    Serial.print("Left Stick Button state is: ");
+    Serial.println(controlPak.StickLButtonValue);
+    Serial.print("Right Stick Button state is: ");
+    Serial.println(controlPak.StickRButtonValue);
+
+    Serial.print("Left Bumper state is: ");
+    Serial.println(controlPak.BumperLValue);
+    Serial.print("Right Bumper state is: ");
+    Serial.println(controlPak.BumperRValue);
+
+    Serial.print("Left Select state is: ");
+    Serial.println(controlPak.SelectLValue);
+    Serial.print("Right Select state is: ");
+    Serial.println(controlPak.SelectRValue);
+
+    Serial.print("Down Button state is: ");
+    Serial.println(controlPak.ButtonDValue);
+    Serial.print("Left Button state is: ");
+    Serial.println(controlPak.ButtonLValue);
+    Serial.print("Right Button state is: ");
+    Serial.println(controlPak.ButtonRValue);
+    Serial.print("Up Button state is: ");
+    Serial.println(controlPak.ButtonUValue);
+*/
+
+ // Drive Commands
+  DriveVel = map(StickLYScaled, -1.00, 1.00, -127.00, 127.00);
+  DriveRot = map(StickLXScaled, -1.00, 1.00, -127.00, 127.00);
+  DiffSteer.computeMotors(DriveVel, DriveRot);
+  LeftMotor = DiffSteer.computedLeftMotor();
+  RightMotor = DiffSteer.computedRightMotor();
+
+    Serial.print("Left Motor value is: ");
+    Serial.println(LeftMotor);
+    Serial.print("Right Motor value is: ");
+    Serial.println(RightMotor);
+
+ // Servo Commands
+ 
+ if ((controlPak.ButtonDValue == LOW)) {
+   servo1.write(65);
+   servo2.write(60);
+   servo3.write(170);
+   servo4.write(175);  
+ }
+
+ else if ((controlPak.ButtonLValue == LOW)) {
+   servo1.write(180);
+   servo2.write(180);
+   servo3.write(170);
+   servo4.write(175);
+ }
+
+ else if ((controlPak.ButtonRValue == LOW)) {
+   servo1.write(65);
+   servo2.write(60);
+   servo3.write(55);
+   servo4.write(55);
+ }
+ 
+ else if ((controlPak.ButtonUValue == LOW)) {
+   servo1.write(180);
+   servo2.write(180);
+   servo3.write(55);
+   servo4.write(55);  
+ }
+
+
+ 
+
+ /* 
   
+  Servo1Value = map(controlPak.StickRXValue, 0, 1024, 0, 180);
+  servo1.write(Servo1Value);
 
-    //end of timed radio loop
-  }
+  Servo2Value = map(controlPak.StickRXValue, 0, 1024, 0, 180);
+  servo2.write(Servo2Value);
 
-  radio.write( &displayPak, sizeof(displayPak) );
+  Servo3Value = map(controlPak.StickRXValue, 0, 1024, 0, 180);
+  servo3.write(180 - Servo3Value);
 
-}
+  Servo4Value = map(controlPak.StickRXValue, 0, 1024, 0, 180);
+  servo4.write(180 - Servo4Value);
+
+*/
+
+}  
--- RF_Controller/RF_Controller.ino
+++ RF_Controller/RF_Controller.ino
@@ -1,30 +1,81 @@
+//// Multifunction RF Controller
+
+// Teensy 4.1 Microcontroller, 4.5V input
+// All Vin to secondary components must be 3.3V
+
+//// Pinout
+// Pin 0: Digital Input, Left Red Button
+// Pin 1: Digital Input, Left Yellow Button
+// Pin 2: Digital Input, Left Blue Button
+// Pin 3: Digital Input, Left White Button
+// Pin 4: Digital Input, Right White Button
+// Pin 5: Digital Input, Right Red Button
+// Pin 6: Digital Input, Right Blue Button
+// Pin 7: Digital Input, Right Yellow Button
+// Pin 9: Digital Output, Radio CE
+// Pin 10: Digital Output, Radio CS
+// Pin 11: SPI, Radio MOSI
+// Pin 12: SPI, Radio MISO
+// Pin 13: SPI, Radio SCK
+// Pin 18: I2C, Screen SDA
+// Pin 19: I2C, Screen SCK
+// Pin 26: Analog In, Right Stick X
+// Pin 27: Analog In, Right Stick Y
+// Pin 28: Digital Input, Right Stick Button
+// Pin 37: Digital Input, Left Stick Button
+// Pin 38: Analog In, Left Stick Y
+// Pin 39: Analog In, Left Stick X
+
+
+
+// Include Libraries and Definitions
+
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
+#include <Wire.h>
+#include <SerLCD.h>
+#include <Bounce.h>
 
-#define joy1button 36
-#define led 32
-#define led_comms_ON 33
-#define led_comms_OFF 34
-#define joy1X A15
-#define joy1Y A14
+#define joy1X A14
+#define joy1Y A15
+#define joy2X A12
+#define joy2Y A13
+#define CE 9
+#define CSN 10
+#define DisplayArrayLength 10
 
-RF24 radio(9, 10, 4000000); // CE, CSN
+
+// Setup button debouncing
+Bounce joy1button = Bounce(37, 10);
+Bounce joy2button = Bounce(28, 10);
+
+Bounce lbYellow = Bounce(1, 10);
+Bounce lbRed = Bounce(0, 10);
+Bounce lbWhite = Bounce(3, 10);
+Bounce lbBlue = Bounce(2, 10);
+
+Bounce rbYellow = Bounce(7, 10);
+Bounce rbRed = Bounce(5, 10);
+Bounce rbWhite = Bounce(4, 10);
+Bounce rbBlue = Bounce(6, 10);
+
+// RF_Controller Radio Setup
+RF24 radio(CE, CSN, 4000000); // CE, CSN
 const byte addresses[][6] = {"00001", "00002"};
 
-boolean buttonStateOut = 0;
-boolean buttonStateIn = 0;
-boolean commState = 0;
+// LCD Setup
+SerLCD lcd; // Initialize with default address 0x72
 
+// Timers
 unsigned long currentMillis;
-long previousMillis = 0;    // set up timers
-long interval = 10;        // time constant for timer
-unsigned long count;
+unsigned long ControllerMillis = 0;
+unsigned long DisplayMillis = 0;
 
-unsigned long ControllerMillis;
+// Variable definitions
+bool commState = 0;
 
-
-
+// Data structure definitions
 typedef struct
 {
   float corrected_angle;
@@ -36,37 +87,79 @@ typedef struct
 {
   int joy1XValue;
   int joy1YValue;
-  boolean joy1buttonValue;
+  int joy1buttonValue;
+  int joy2XValue;
+  int joy2YValue;
+  int joy2buttonValue;
+  int lbYellowValue;
+  int lbRedValue;
+  int lbWhiteValue;
+  int lbBlueValue;
+  int rbYellowValue;
+  int rbRedValue;
+  int rbWhiteValue;
+  int rbBlueValue;
 }
 controlDef;
 controlDef controlPak;
 
 void setup() {
-  pinMode(32, OUTPUT);
-  pinMode(33, OUTPUT);
-  pinMode(34, OUTPUT);
-  pinMode(joy1button, INPUT_PULLUP);
 
+
+
+// Set Pins  
+  pinMode(37, INPUT_PULLUP); //joy1button
+  pinMode(28, INPUT_PULLUP); //joy2button
+  pinMode(7, INPUT_PULLUP); //rbYellow
+  pinMode(5, INPUT_PULLUP); //rbRed
+  pinMode(4, INPUT_PULLUP); //rbWhite
+  pinMode(6, INPUT_PULLUP); //rbBlue
+  pinMode(1, INPUT_PULLUP); //lbYellow
+  pinMode(0, INPUT_PULLUP); //lbRed
+  pinMode(3, INPUT_PULLUP); //lbWhite
+  pinMode(2, INPUT_PULLUP); //lbBlue
+
+
+// Initialize I2C to Screen @ 400kHz
+  Wire.begin();
+  lcd.begin(Wire);
+  Wire.setClock(400000);
+  delay(2000);
+  lcd.clear();
+  lcd.print("I2C Online");
+  delay(1000);
+//  lcd.autoscroll();
+
+// Initialize Teensy-to-PC Serial
   Serial.begin(115200);
+  lcd.clear();  
+  lcd.print("Serial Online");
+  delay(1000);
 
+// Initialize Radio
   radio.begin();
   radio.openWritingPipe(addresses[0]); // 00001
   radio.openReadingPipe(1, addresses[1]); // 00002
-  radio.setPALevel(RF24_PA_MAX);
+  radio.setPALevel(RF24_PA_LOW);
   radio.setChannel(100);
+  lcd.clear();  
+  lcd.println("Radio Online");
+  delay(1000);
+  lcd.clear();
 }
+
+
 
 void loop() {
 
-  currentMillis = millis();
-  if (currentMillis - previousMillis >= 10) {  // start timed event
+// Timer Start
+currentMillis = millis();
 
-    previousMillis = currentMillis;
 
-  delay(5);
+// Retrieve displayPak Data  
   radio.startListening();
 
-      // check for radio data
+      // Check for Radio Data
   if (radio.available()) {
       radio.read(&displayPak, sizeof(displayPak));
       ControllerMillis = currentMillis;
@@ -74,42 +167,70 @@ void loop() {
       Serial.println(displayPak.corrected_angle);
     }
 
-
-
-    // is the remote disconnected for too long ?
-  if (currentMillis - ControllerMillis > 500) {
+    // Radio Timeout Check
+  if ((currentMillis - ControllerMillis) > 500) {
       commState = 0;
-      Serial.println("no data");
-  }
-  else {
-    commState = 1;      
   }
 
-  if (buttonStateIn == HIGH) {
-    digitalWrite(led, HIGH);
-  }
-  else {
-    digitalWrite(led, LOW);
+  else if((currentMillis - ControllerMillis) < 500) {
+    commState = 1;     
   }
 
-  if (commState == 1) {
-    digitalWrite(led_comms_ON, HIGH);
-    digitalWrite(led_comms_OFF, LOW);
-  }
-  else {
-    digitalWrite(led_comms_OFF, HIGH);
-    digitalWrite(led_comms_ON, LOW);
-  }
+// Switch Radio Modes
+  radio.stopListening();
   delay(5);
 
-  radio.stopListening();
-  }
+// Update Buttons
+  joy1button.update();
+  joy2button.update();
+  lbYellow.update();
+  lbRed.update();
+  lbWhite.update();
+  lbBlue.update();
+  rbYellow.update();
+  rbRed.update();
+  rbWhite.update();
+  rbBlue.update();
 
-  controlPak.joy1buttonValue = digitalRead(joy1button);
+
+// Update controlPak Data
   controlPak.joy1XValue = analogRead(joy1X);
   controlPak.joy1YValue = analogRead(joy1Y);
-  
+  controlPak.joy1buttonValue = joy1button.read();
+  controlPak.joy2XValue = analogRead(joy2X);
+  controlPak.joy2YValue = analogRead(joy2Y);
+  controlPak.joy2buttonValue = joy2button.read();
+  controlPak.lbYellowValue = lbYellow.read();
+  controlPak.lbRedValue = lbRed.read();
+  controlPak.lbWhiteValue = lbWhite.read();
+  controlPak.lbBlueValue = lbBlue.read();
+  controlPak.rbYellowValue = rbYellow.read();
+  controlPak.rbRedValue = rbRed.read();
+  controlPak.rbWhiteValue = rbWhite.read();
+  controlPak.rbBlueValue = rbBlue.read();
+
+// Send controlPak Data
   radio.write(&controlPak, sizeof(controlPak));
-    //end of timed radio loop
-  
+
+// Update Display
+  if ((commState = 0)){
+    lcd.println("No Comms");
+    Serial.println("No Comms");
+  }
+  else if ((commState = 1)){
+    lcd.println("Radio Connected");
+    Serial.println("Radio Connected");
+  }
+ // else {  
+ //   lcd.println(" ");
+ // }
+
+  int displayOutputs[DisplayArrayLength] = {controlPak.joy1buttonValue, controlPak.joy2buttonValue, controlPak.lbYellowValue, controlPak.lbRedValue, controlPak.lbWhiteValue, controlPak.lbBlueValue, controlPak.rbYellowValue, controlPak.rbRedValue, controlPak.rbWhiteValue, controlPak.rbBlueValue};
+  //for (int x = 0; x < DisplayArrayLength; x++){
+  //lcd.print(displayOutputs[x]);
+ // }
+  //lcd.println();
+  Serial.println(currentMillis);
+  Serial.println(ControllerMillis);
+
 }
